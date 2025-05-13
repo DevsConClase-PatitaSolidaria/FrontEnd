ab5318ff33700766c2f8bd207dda1c29e6058d91
Diegoroip <rosadoiporre@gmail.com>
2025-05-12 21:57:15 -0500

    updating files for frontend

---
+++ src/app/donations/pages/donation-options/donation-options.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donation-options',
+  templateUrl: './donation-options.component.html',
+  styleUrls: ['./donation-options.component.css']
+})
+export class DonationOptionsComponent {}
---
+++ src/app/pets/pages/pets-gallery/pets-gallery.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pets-gallery',
+  templateUrl: './pets-gallery.component.html',
+  styleUrls: ['./pets-gallery.component.css']
+})
+export class PetsGalleryComponent {}
