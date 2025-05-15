6c4766635fa5a7313b97d65c147393860762a815
rhylow <leccapedro8@gmail.com>
2025-05-15 13:24:29 -0500

    feat: add internationalization support

--- src/app/donations/pages/donation-options/donation-options.component.ts
+++ src/app/donations/pages/donation-options/donation-options.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Donation } from '../../model/donation.entity';
 import { DonationsService } from '../../services/donations.service';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 import { DonationCardComponent } from '../../components/donation-card/donation-card.component';
 
 type UserRole = 'refugio' | 'adoptante' | 'rescatista';
@@ -9,7 +10,7 @@ type UserRole = 'refugio' | 'adoptante' | 'rescatista';
 @Component({
   selector: 'app-donation-options',
   standalone: true,
-  imports: [CommonModule, DonationCardComponent],
+  imports: [CommonModule, TranslateModule, DonationCardComponent],
   templateUrl: './donation-options.component.html',
   styleUrls: ['./donation-options.component.css']
 })
@@ -39,5 +40,3 @@ export class DonationOptionsComponent implements OnInit {
   }
 
 }
-
-
