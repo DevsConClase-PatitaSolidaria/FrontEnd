edb7d780da10b906ec19bc73cca4d0ca77ec4323
rhylow <leccapedro8@gmail.com>
2025-05-15 14:11:56 -0500

    feat: update user-info.component.ts

--- src/app/profile/components/user-info/user-info.component.ts
+++ src/app/profile/components/user-info/user-info.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../../model/user.entity';
 import {TitleCasePipe} from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
   imports: [
-    TitleCasePipe
+    TitleCasePipe,
+    TranslateModule
   ],
   styleUrls: ['./user-info.component.css']
 })
