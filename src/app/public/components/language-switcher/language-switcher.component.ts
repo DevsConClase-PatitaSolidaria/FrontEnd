e1979ead879e45fcf887926b7b3471e1698fd6b0
rhylow <leccapedro8@gmail.com>
2025-05-15 10:33:09 -0500

    fix: update language-switcher

--- src/app/public/components/language-switcher/language-switcher.component.ts
+++ src/app/public/components/language-switcher/language-switcher.component.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { CommonModule } from '@angular/common'; // Importa CommonModule
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-language-switcher',
   standalone: true,
-  imports: [MatButtonToggleModule, CommonModule], // Agrega CommonModule aquí
   templateUrl: './language-switcher.component.html',
-  styleUrls: ['./language-switcher.component.css'] // Corrige "styleUrl" a "styleUrls"
+  styleUrls: ['./language-switcher.component.css'],
+  imports: [
+    CommonModule,
+    MatButtonToggleModule,
+    TranslateModule
+  ]
 })
+
 export class LanguageSwitcherComponent {
   currentLang: string = 'en';
   languages: string[] = ['en', 'es'];
 
   constructor(private translate: TranslateService) {
-    this.currentLang = translate.currentLang;
+    this.currentLang = this.translate.currentLang || this.translate.getDefaultLang();
+    this.translate.onLangChange.subscribe(event => {
+      this.currentLang = event.lang;
+    });
   }
 
   useLanguage(language: string): void {
